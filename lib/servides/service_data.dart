6324a5259c21b8b60e2680851f3da94af6bff1b6
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-23 11:56:13 -0500

    service data

--- lib/servides/service_data.dart
+++ lib/servides/service_data.dart
@@ -1,1 +1,16 @@
-class ServiceData {}
+import 'package:travelappg8/models/country_model.dart';
+import 'package:travelappg8/models/tour_model.dart';
+
+class ServiceData {
+  Future<List<CountryModel>> getCountries() {
+    return Future.delayed(Duration(seconds: 3), () {
+      return [peru, chile, argentina, venezuela];
+    });
+  }
+
+  Future<List<TourModel>> getPopularTours() {
+    return Future.delayed(Duration(seconds: 5), () {
+      return [tour1, tour2, tour3, tour4, tour1, tour2, tour3];
+    });
+  }
+}
