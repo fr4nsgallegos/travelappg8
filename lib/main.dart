608e7c3de862f34e9671a23d0e9d12d125fbf9e8
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-23 09:24:43 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:travelappg8/pages/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
